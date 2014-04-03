8b25f3038891255a9558b4d09ad07836e85217ff
Dan Smith <dsmith@danplanet.com>
2014-04-03 09:01:07 -0700

    Wrap text in the description textview

--- ical_reminder/dialog.py
+++ ical_reminder/dialog.py
@@ -21,6 +21,7 @@ class Reminder(Gtk.Dialog):
         vbox.add(self.detail_label)
 
         description = Gtk.TextView()
+        description.set_wrap_mode(Gtk.WrapMode.WORD)
         description.get_buffer().set_text(
             'Location: %s\n\n%s' % (
                 event.location, event.description))
@@ -87,7 +88,10 @@ if __name__ == '__main__':
     event.start_time = datetime.datetime.now()
     event.summary = 'Test Event'
     event.location = 'Test Location'
-    event.description = 'Loooooooooooooooooooots\nOf\nDetails!'
+    event.description = ('Loooooooooooooooooooots\nOf\nDetails!\n' +
+                         'this will become a very long line which ' +
+                         'will help test that the wrapping mode is ' +
+                         'correctly set on the text view.')
 
     alarm = Alarm()
 
